45430ab9fe068f8bc1a0b590d9670061b8d346be
Reece Hart <reecehart@gmail.com>
2020-07-18 11:56:55 -0700

    voteamerica backend was really vote.org :-/

--- src/voter_reg_check/backends/__init__.py
+++ src/voter_reg_check/backends/__init__.py
@@ -1,9 +1,9 @@
-from .voteamerica import check as check_voteamerica
+from .voteorg import check as check_voteorg
 from .rockthevote import check as check_rockthevote
 
 checkers = {
     "rockthevote": check_rockthevote,
-    "voteamerica": check_voteamerica
+    "voteorg": check_voteorg
     }
 
 def check(method, voterinfo):
